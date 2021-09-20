f6eea12ca9c8a3e90f45026858871362f19eb1d5
Migue1409-KP <76017236+Migue1409-KP@users.noreply.github.com>
2021-09-20 01:08:22 -0500

    update the app

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const { argv } = require('process');
 const optionSelect = require('./taskFunction')
 
 //execute the code and receive argument to options execute
-optionSelect(argv[2]);
+let option = argv[2];
+argv.splice(0,3);
+optionSelect(option, argv);
--- taskFunction.js
+++ taskFunction.js
@@ -3,20 +3,77 @@ let fs = require('fs');
 
 //read the file JSON
 let readToDo = () => {
-  let file = fs.readFileSync("./tareas.json", "utf-8");
-  file = JSON.parse(file);
-  return file;
+  let file = fs.readFileSync("./tasks.json", "utf-8");
+  return JSON.parse(file);
 };
 
-//print the list and esthetic:    
-let listToDo = () => {
-  fileList = readToDo();
+//function to upload list of objects to JSON file
+let saveToDo = (fileListArray) => {
+  fileListArray = JSON.stringify(fileListArray, null, 4);
+  fs.writeFileSync("./tasks.json", fileListArray, {encoding: "utf-8", flag: 'w'})
+}
+
+//convert array to string
+let arrayToString = (array) => {
+  let string = array.reduce(function(acum, num){
+    return acum + " " + num; 
+  })
+  return string;
+}
+
+//receives the option and execute 
+let optionSelect = (option, rest) => {
+  let otherArgument = "";
+  switch (option) {
+    case "toList":
+    case "listar":
+      listToDo(readToDo());
+      break;
+
+    case "crear":
+    case "create":
+      otherArgument = arrayToString(rest);
+      newToDo(otherArgument);
+      break;
+    
+    case "filter":
+    case "filtrar":
+      otherArgument = arrayToString(rest);
+      listToDo(filterForStatus(otherArgument));
+      break;
+    
+    case undefined:
+      console.log("Attention: you have to pass an action");
+      break;
+
+    default:
+      console.log("I don't understand what you want to do");
+  }
+};
+
+//function for create new task:
+let newToDo = (titleTask) => {
+  let fileList = readToDo();
+  fileList.push({title: titleTask, status: "Pending"});
+  saveToDo(fileList);
+}
+
+//function for print by status:
+let filterForStatus = (parameter) => {
+  let fileList = readToDo();
+  let fileListFilter = fileList.filter(function(element){
+    return element.status == parameter;
+  })
+  return fileListFilter;
+}
+
+//function for print
+let listToDo = (arrayList) => {
   let status = "";
   console.log("\nNote:    |✓| => Finished,  |...| => In progress,   |X| => Pending");
   console.log("LIST TO-DO: \n");
-
-  for (let i = 0; i < fileList.length; i++) {
-    switch (fileList[i].status) {
+  arrayList.forEach(function(line){
+    switch (line.status) {
       case "Finished":
         status = "| ✓ |";
         break;
@@ -33,26 +90,10 @@ let listToDo = () => {
         status = "| X |";
     }
 
-    console.log(status + " " + fileList[i].title);
-  }
+    console.log(status + " " + line.title);
+  })
 };
 
-//receives the option and execute 
-let optionSelect = (option) => {
-  switch (option) {
-    case "to list":
-    case "listar":
-      listToDo();
-      break;
-
-    case undefined:
-      console.log("Atención - Tienes que pasar una acción");
-      break;
-
-    default:
-      console.log("No entiendo qué quieres hacer");
-  }
-};
 
 //export the main module
 module.exports = optionSelect;
